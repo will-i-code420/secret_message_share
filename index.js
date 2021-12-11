ae5b273add42ca690b2d09b32929e13a08cecd76
Will Aronow <waronow123@gmail.com>
2021-12-10 19:38:13 -0700

    first commit, set up basic html and msg input and link display

---
+++ index.js
@@ -0,0 +1,10 @@
+document.querySelector('form').addEventListener('submit', (e) => {
+	e.preventDefault();
+	const msgInput = document.querySelector('#message-input');
+	const encoded = btoa(msgInput.value);
+	const linkInput = document.querySelector('#link-input');
+	msgInput.value = '';
+	document.querySelector('#message-input-container').classList.add('hidden');
+	linkInput.value = encoded;
+	document.querySelector('#link-input-container').classList.remove('hidden');
+});
